99e17635f1e65a6fbf5b2075a699466429d07b2c
gadielkalleb <gadiel_kalleb@hotmail.com>
2021-10-04 16:35:04 -0300

    refactor: add logic to log in table format

--- src/index.js
+++ src/index.js
@@ -1,66 +1,28 @@
-import fetch from 'node-fetch';
-import chalk from 'chalk';
-
-const log = console.log;
-const logEnter = (text) => {
-	log(text);
-	log();
-};
-
-const invalidParamsObjectError = () => { throw new Error('Parâmetro de Objeto inválido') }
-
-const iconByStatus = {
-	TRANSITO: '🚚',
-	'SAIU-ENTREGA-DESTINATARIO': '🙌',
-	ENTREGUE: '🎁',
-	PAR17: '💸', //Aguardando pagamento
-	PAR21: '🔎', //Encaminhado para fiscalização aduaneira
-	RecebidoCorreiosBrasil: '🛬',
-	POSTAGEM: '📦',
-	DEFAULT: '🚧',
-};
-
-function getIcon(status) {
-	return iconByStatus[status] || iconByStatus.DEFAULT;
-}
-
-async function getData(code) {
-	const url = 'https://rastreamento.correios.com.br/app/resultado.php';
-
-	const response = await fetch(`${url}?objeto=${code}&mqs=S`);
-	const data = await response.json();
-
-	return data;
-}
-
-const logEventData = ({ descricao, descricaoWeb, dtHrCriado, unidade, unidadeDestino }) => {
-	console.table(descricao, descricaoWeb, dtHrCriado, unidade, unidadeDestino)
-	// log(`==> ${getIcon(descricaoWeb)} ${descricao}`);
-	// log(chalk.blackBright(`Data: ${dtHrCriado}`));
-	// log(chalk.blackBright(`Local: ${unidade.nome}`));
-
-	// if (unidadeDestino) {
-	// 	log(chalk.blackBright(`Indo para: ${unidadeDestino?.nome}`));
-	// }
-
-	log();
-}
+import { getData } from './services/index.js';
+import { log, logEnter, invalidParamsObjectError, trackingObjectTable, logEventData, boldText } from './utils/index.js'
 
 async function run() {
 	try {
 		const code = process?.argv[2]?.toUpperCase() ?? invalidParamsObjectError()
+		const table = process?.argv[3]?.toUpperCase() ?? null
 
-		logEnter(chalk.bold(`📮 ${code}`));
+		logEnter(boldText(`📮 ${code}`));
 
 		const data = await getData(code);
 
 		if (!!data.erro) {
-			throw new Error(data.mensagem)
+			throw new Error(data.mensagem);
 		}
 
 		const events = data?.eventos ?? [];
 
+		if (!!table) {
+			console.table(events.map(trackingObjectTable));
+			return;
+		}
+
 		events.map(logEventData);
+		return;
 
 	} catch (error) {
 		log(`❌ ${error.message}`);
